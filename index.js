6c3355ae02638789e2b81437b02baf112c1489cf
philipodev <philipodev@gmail.com>
2018-03-26 15:31:45 +0200

    change to ts-node

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const path = require('path');
 
-module.exports = function(events) {
-  events.on('webpack-config', function(config) {
-    config.resolveLoader.modules.push(path.resolve(__dirname, 'node_modules'));
-    config.module.rules.push({
-      test: /\.tsx?$/,
-      loader: 'ts-loader',
-      options: {
-        configFile: path.resolve(process.cwd(), './.tsconfig.json'),
-        compilerOptions: {
-          experimentalDecorators: true,
-          sourceMap: false
-        }
-      }
-    });
+module.exports = function() {
+  require('ts-node').register({
+    configFile: path.resolve(process.cwd(), './.tsconfig.json'),
+    cache: true,
+    compilerOptions: {
+      module: 'commonjs',
+      experimentalDecorators: true,
+      sourceMap: false,
+      target: 'es6'
+    }
   });
 };
